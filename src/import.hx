e127dc4f041b0e091ac6789d8c04f6d3f2e43095
tong <tong@disktree.net>
2018-01-19 20:59:14 +0100

    Add import.hx

---
+++ src/import.hx
@@ -0,0 +1,22 @@
+
+import om.Json;
+
+using om.StringTools;
+
+#if js
+
+import js.Browser.console;
+import js.Browser.document;
+import js.Browser.navigator;
+import js.Browser.window;
+import js.html.Element;
+import om.DOM;
+import om.Tween;
+import om.ease.*;
+
+using om.DOM;
+using om.StringTools;
+
+//#elseif sys
+
+#end
