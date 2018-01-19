ce26b3404fc2e53c9775856ec54e075f16767489
tong <tong@disktree.net>
2018-01-19 21:42:06 +0100

    Split html/body template content

--- src/drylungs/Web.hx
+++ src/drylungs/Web.hx
@@ -10,22 +10,18 @@ class Web {
 
     static inline var ROOT = '/project/drylungs.at/bin/';
 
+    public static var site(default,null) : Dynamic;
+
     function new() {}
 
     function doDefault( ?path : String ) {
-
-        printHTML();
-        
-        /*
-        if( path == null || path.length == 0 || path == 'index.php' ) {
-            printHTML();
-        } else {
-            handleError( 404, 'not found' );
+        switch path {
+        case null,'','/','start':
+            printSite( path );
+        default:
+            printSite( 'error', { code: 404, message: 'not found' } );
         }
-        */
     }
-
-    //function doStyle() {
 
     function doAtom() {
         php.Web.setHeader( 'Content-Type', 'application/atom+xml' );
@@ -42,34 +38,57 @@ class Web {
         //printTemplate( Resource.getString( 'manifest' ) );
     }
 
-    static function printHTML( id = 'index', ?ctx : Dynamic ) {
-        Sys.print( executeTemplate( Resource.getString( id ), ctx ) );
+    static function printSite( id : String, ?context : Dynamic ) {
+
+        var ctx : Dynamic = {};
+        for( f in Reflect.fields( site ) ) {
+            Reflect.setField( ctx, f, Reflect.field( site, f ) );
+        }
+        copyFields( site, ctx );
+        if( context != null ) {
+            copyFields( context, ctx );
+        }
+
+        var body = executeTemplate( Resource.getString( id ), ctx );
+        ctx.body = body;
+
+        var html = executeTemplate( Resource.getString( 'index' ), ctx );
+        Sys.print( html );
     }
 
-    static function executeTemplate( str : String, ?ctx : Dynamic ) {
+    static function executeTemplate( str : String, ?ctx : Dynamic ) : String {
         if( ctx == null ) ctx = {};
         return new Template( str ).execute( ctx );
     }
 
-    static function handleError( code : Int, message = '') {
-        printHTML( 'error', { code: code, message: message } );
+    static function copyFields<A,B>( src : A, dst : Dynamic ) {
+        for( f in Reflect.fields( src ) ) {
+            Reflect.setField( dst, f, Reflect.field( src, f ) );
+        }
     }
 
     static function main() {
 
         //Session.start();
 
+        site = Json.parse( File.getContent( 'content/site.json' ) );
+
         var uri = php.Web.getURI();
         var path = php.Web.getURI().substr( ROOT.length );
         var params = php.Web.getParams();
         var mobile = om.Runtime.isMobile();
 
-        var releases = Json.parse( File.getContent( 'content/releases.json' ) );
-        releases.reverse();
+        if( path.length == 0 ) path = 'start';
 
-        Template.globals = Json.parse( File.getContent( 'content/site.json' ) );
+        Template.globals = site;
+        Template.globals.path = path;
         Template.globals.mobile = mobile;
         Template.globals.device = mobile ? 'mobile' : 'desktop';
+
+        site.path = path;
+
+        var releases = Json.parse( File.getContent( 'content/releases.json' ) );
+        releases.reverse();
         Template.globals.releases = releases;
 
         var dispatcher = new Dispatch( path, params );
