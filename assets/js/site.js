23783d1324b92dc7b37a41989469a083448da787
tong <tong@disktree.net>
2022-05-02 11:05:18 +0200

    Minor tweaks

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,42 +1,42 @@
 'use strict'
 
-console.info('%c⛧ DRYLUNGS RECORDS ⛧', 'background:#000;color:#fff;padding:4px;font-size:23px;');
-
 let menu, nav, main;
 
 function toggleNav() {
-	menu.classList.toggle( 'is-active' );
-    nav.classList.toggle( 'show' );
-    main.classList.toggle( 'blur' );
+    menu.classList.toggle('is-active');
+    nav.classList.toggle('show');
+    main.classList.toggle('blur');
 }
 
-window.addEventListener('load', e => {
+window.addEventListener('load', _ => {
+
+    console.info('%c⛧ DRYLUNGS RECORDS ⛧', 'background:#000;color:#fff;padding:4px;font-size:23px;');
 
     const header = document.body.querySelector('header');
-    var title = header.querySelector('.title');
+    const title = header.querySelector('.title');
+	const page = header.querySelector('.page');
     nav = document.body.querySelector('nav');
     main = document.body.querySelector('main');
     menu = header.querySelector('.menu');
-	var page = header.querySelector('.page');
 
-	menu.onclick = e => {
-		toggleNav();
-	};
-	title.onclick = e => {
-		toggleNav();
-	};
-	page.onclick = e => {
-		toggleNav();
-	};
-	main.onclick = e => {
-		if( main.classList.contains('blur') ) {
-			toggleNav();
-		}
-	};
+    menu.onclick = _ => {
+        toggleNav();
+    };
+    title.onclick = _ => {
+        toggleNav();
+    };
+    page.onclick = _ => {
+        toggleNav();
+    };
+    main.onclick = _ => {
+        if (main.classList.contains('blur')) {
+            toggleNav();
+        }
+    };
 
-	window.addEventListener( 'keydown', e => {
-		if( e.keyCode === 27 ) {
-			toggleNav();
-		}
-	}, false );
-});
+    window.addEventListener('keydown', e => {
+        if (e.keyCode === 27) {
+            toggleNav();
+        }
+    }, false);
+}, false);
