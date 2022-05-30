653c4a18e0932d4cff7e30502272b0055249296f
tong <tong@disktree.net>
2022-05-30 13:46:08 +0200

    Navigate to intro page on desktop devices

--- assets/js/site.js
+++ assets/js/site.js
@@ -6,42 +6,43 @@ const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigat
 let menu, nav, main;
 
 function toggleNav() {
-    menu.classList.toggle('is-active');
-    nav.classList.toggle('show');
-    main.classList.toggle('blur');
+    if (isMobileDevice) {
+        menu.classList.toggle('is-active');
+        nav.classList.toggle('show');
+        main.classList.toggle('blur')
+    } else {
+        location.href = "/";
+    }
 }
 
 window.addEventListener('load', _ => {
 
-    console.info('%c⛧ DRYLUNGS RECORDS ⛧', 'background:#000;color:#fff;padding:4px;font-size:23px;');
+    console.info('%c⛧  DRYLUNGS RECORDS ⛧', 'background:#000;color:#fff;padding:4px;font-size:23px;');
 
-    if (isMobileDevice) {
-        const header = document.body.querySelector('header');
-        const title = header.querySelector('.title');
-        const page = header.querySelector('.page');
-        nav = document.body.querySelector('nav');
-        main = document.body.querySelector('main');
-        menu = header.querySelector('.menu');
+    const header = document.body.querySelector('header');
+    const title = header.querySelector('.title');
+    const page = header.querySelector('.page');
+    nav = document.body.querySelector('nav');
+    main = document.body.querySelector('main');
+    menu = header.querySelector('.menu');
 
-        menu.onclick = _ => {
+    menu.onclick = _ => {
+        toggleNav();
+    };
+    title.onclick = _ => {
+        toggleNav();
+    };
+    page.onclick = _ => {
+        toggleNav();
+    };
+    main.onclick = _ => {
+        if (main.classList.contains('blur')) {
             toggleNav();
-        };
-        title.onclick = _ => {
+        }
+    };
+    window.addEventListener('keydown', e => {
+        if (e.code === 'Escape') {
             toggleNav();
-        };
-        page.onclick = _ => {
-            toggleNav();
-        };
-        main.onclick = _ => {
-            if (main.classList.contains('blur')) {
-                toggleNav();
-            }
-        };
-        window.addEventListener('keydown', e => {
-            console.log(e);
-            if (e.code === 'Escape') {
-                toggleNav();
-            }
-        }, false);
-    }
+        }
+    }, false);
 }, false);
